5be8e4859dedacc0b68cca2887380ff9a84b4843
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-07-13 13:23:51 -0300

    update

--- script/informacoesGerais.js
+++ script/informacoesGerais.js
@@ -2,11 +2,17 @@ var json = {
   "sobre": [
     {
       "conteudo": "O Mercado IFPE é fruto de um projeto desenvolvido perante à cadeira de Desenvolvimento Web1 no curso de Análise e Desenvolvimento de Sistemas"
+    },
+    {
+      "tipo": "Sobre"
     }
   ],
   "contato": [
     {
       "conteudo": "Você pode entrar em contato conosco através do número de telefone: (81) 9 9999-9999!"
+    },
+    {
+      "tipo": "Contato"
     }
   ]
 };
--- script/modalTelaInicial.js
+++ script/modalTelaInicial.js
@@ -1,10 +1,12 @@
-const abrirModal = (conteudo) => {
+const abrirModal = (conteudo, tipo) => {
   document.querySelector('.modalWindowArea').style.opacity = 0; // transparente
   document.querySelector('.modalWindowArea').style.display = 'flex';
   setTimeout(() => document.querySelector('.modalWindowArea').style.opacity = 1, 150);
 
+  var divTipo = document.getElementById('tipo');
   var divText = document.getElementById('text');
   divText.textContent = json[conteudo][0].conteudo;
+  divTipo.textContent = json[conteudo][1].tipo;
 }
 
 const fecharModal = () => {
