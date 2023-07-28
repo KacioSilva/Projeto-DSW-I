ecd9d0a79075fd13f98c211786f855ec944d4043
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-07-28 01:05:16 -0300

    update

--- script/transicaoImagens.js
+++ script/transicaoImagens.js
@@ -1,11 +1,3 @@
 function slider (anything){
     document.querySelector ('.celularAmostra') .src = anything;
 };
-
-let menu = document.querySelector ('#menu-icon');
-let navbar = document.querySelector ('.navbar');
-
-menu.onclick = () => {
-menu.classList.toggle ('bx-x');
-navbar.classList.toggle ('open');
-}
